c498b3d10448475f938c67103a8f39cf1b00bba2
josoya <thdi4564@nate.com>
2023-02-27 16:08:12 +0900

    spring 설정 및 vue 설정

---
+++ src/main/java/com/soya/backend/WebErrorController.java
@@ -0,0 +1,15 @@
+package com.soya.backend;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class WebErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String redirectRoot() {
+        return "index.html";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+}
