046c5826d5afbe491d1a2e96bdb0e0667cb7556c
josoya <thdi4564@nate.com>
2023-03-28 16:45:24 +0900

    jpa 및 querydsl 추가

--- src/main/java/com/soya/backend/BackendApplication.java
+++ src/main/java/com/soya/backend/BackendApplication.java
@@ -10,4 +10,8 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+
+
+
 }
+
--- src/main/java/com/soya/backend/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soya.backend.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository{
-
-}
